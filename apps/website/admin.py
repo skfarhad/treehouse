26f31aa6f8b2a58f346f7be21b5661a4792444d7
SK Farhad <codeman.farhad@gmail.com>
2018-09-28 22:17:24 +0600

    Added image to paragraph

--- apps/website/admin.py
+++ apps/website/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Paragraph, TextField
 
-# Register your models here.
+
+class ParagraphAdmin(admin.ModelAdmin):
+    # readonly_fields = ['rating',]
+    ordering = ('-timestamp',)
+    list_filter = (
+        'type',
+    )
+    search_fields = [
+        'title',
+    ]
+    list_display = ('type',
+                    'title',
+                    'timestamp',
+                    'text',
+                    )
+
+
+class TextFieldAdmin(admin.ModelAdmin):
+
+    list_display = ('type',
+                    'text',
+                    )
+
+
+admin.site.register(Paragraph, ParagraphAdmin)
+admin.site.register(TextField, TextFieldAdmin)
--- apps/website/models.py
+++ apps/website/models.py
@@ -6,7 +6,7 @@ from apps.website.config import PARA_TYPES, TEXT_TYPES
 class Paragraph(models.Model):
     type = models.PositiveSmallIntegerField(default=0, choices=PARA_TYPES)
     title = models.CharField(max_length=256, blank=False)
-    image = models.URLField()
+    image = models.ImageField(upload_to="para_images/", blank=True, null=True)
     timestamp = models.DateTimeField(default=now)
 
     text = models.TextField(blank=True)
