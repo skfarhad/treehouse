bc26504db286182f7ed00c6a1251771d6ac69b45
SK Farhad <codeman.farhad@gmail.com>
2019-10-06 00:17:33 +0600

    Added url param to image field

--- apps/website/models.py
+++ apps/website/models.py
@@ -7,7 +7,7 @@ class Paragraph(models.Model):
     type = models.PositiveSmallIntegerField(default=0, choices=PARA_TYPES)
     title = models.CharField(max_length=256, blank=True)
     text = models.TextField(blank=True)
-    image = models.ImageField(upload_to="para_images/", blank=True, null=True)
+    image_path = models.ImageField(upload_to="para_images/", blank=True, null=True)
     timestamp = models.DateTimeField(default=now)
 
 
