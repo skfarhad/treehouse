655d148af566117b5335ebd2f8ef4c8e0782ba12
sk_farhad <sk.farhad.eee@gmail.com>
2021-03-16 23:09:07 +0600

    Added Image field

--- apps/website/admin.py
+++ apps/website/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Paragraph, TextField
+from .models import Paragraph, TextField, ImageField
 
 
 class ParagraphAdmin(admin.ModelAdmin):
@@ -25,5 +25,13 @@ class TextFieldAdmin(admin.ModelAdmin):
                     )
 
 
+class ImageFieldAdmin(admin.ModelAdmin):
+
+    list_display = ('title',
+                    'image_path',
+                    )
+
+
 admin.site.register(Paragraph, ParagraphAdmin)
 admin.site.register(TextField, TextFieldAdmin)
+admin.site.register(ImageField, ImageFieldAdmin)
--- apps/website/models.py
+++ apps/website/models.py
@@ -14,3 +14,8 @@ class Paragraph(models.Model):
 class TextField(models.Model):
     type = models.PositiveSmallIntegerField(default=0, choices=TEXT_TYPES)
     text = models.CharField(max_length=512, blank=False)
+
+
+class ImageField(models.Model):
+    title = models.CharField(max_length=256, blank=True)
+    image_path = models.ImageField(upload_to="para_images/", blank=True, null=True)
